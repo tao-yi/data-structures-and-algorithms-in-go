d026610ddf127d2a0b14b7a83c06704a56a0fe78
Tao Yi <tao.yi@treelab.co>
2021-02-20 19:10:25 +0800

    add trie

--- data_structures/trie/trie.go
+++ data_structures/trie/trie.go
@@ -1,1 +1,19 @@
 package trie
+
+type trieNode struct {
+	children    []*trieNode
+	isEndOfWord bool
+}
+
+func NewTrieNode() *trieNode {
+	return &trieNode{}
+}
+
+// space efficient
+// efficient prefix queries
+type Trie interface {
+	Insert()
+}
+type trie struct {
+	root *trieNode
+}
